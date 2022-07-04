5bc30a4a5b1e759d9f71b85ba73fb2af28f5efe8
joaoh_mota <joaohenriquemota1@gmail.com>
2022-07-04 17:20:38 -0300

    Sidebar almost done

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -6,6 +6,9 @@ import GoogleLogin from 'react-google-login';
 import { AiFillHome, AiOutlineMenu } from 'react-icons/ai';
 import { ImCancelCircle } from 'react-icons/im';
 import { useState } from 'react';
+import Discover from './Discover';
+import SuggestedAccounts from './SuggestedAccounts';
+import Footer from './Footer';
 
 const Sidebar = () => {
 
@@ -62,7 +65,9 @@ const Sidebar = () => {
                             </div>
                         </div>
                     )}
-
+                    <Discover />
+                    <SuggestedAccounts />
+                    <Footer />
                 </div>
             )}
         </div>
